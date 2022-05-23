6a22536f2e3696e0287bb54bb617cc48bf6236c9
Jakku <J97k107u45k117u@gmail.com>
2022-05-22 22:50:21 -0400

    Initial Server. Adding DB connection support.

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const fileupload = require("express-fileupload");
+
+// Setup:
+const app = express();
+app.set("port", process.env.PORT || 3000);
+app.use(express.static(path.join(__dirname, "public")));
+
+// Midlewares:
+app.use(fileupload());
+app.use((req, res, next) => {
+	console.log(`[${req.method.toUpperCase()}] ${req.protocol.toLowerCase()}://localhost:${app.get("port")}${req.url}`);
+	next();
+});
+
+// Routes:
+
+
+// Startup:
+app.listen(app.get("port"), () => {
+	console.log(`Server on port ${app.get("port")}`);
+});
